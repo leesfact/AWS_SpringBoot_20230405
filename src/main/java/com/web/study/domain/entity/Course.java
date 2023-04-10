cf15c02394873f705c76c3b85f4d62a2d312c9c3
leesfact <lky110408@gmail.com>
2023-04-11 07:44:27 +0900

    23.04.11 domain.entity.Course 누락

---
+++ src/main/java/com/web/study/domain/entity/Course.java
@@ -0,0 +1,19 @@
+package com.web.study.domain.entity;
+
+import java.time.LocalDate;
+
+import lombok.Builder;
+import lombok.Getter;
+
+
+@Builder
+@Getter
+public class Course {
+	private int id;
+	private int lecture_id;
+	private int student_id;
+	private LocalDate registe_date;
+	private Lecture lecture;
+	private Student student;
+
+}
