ee3445c0395b55d628e231746b84651695101bac
leesfact <lky110408@gmail.com>
2023-04-14 17:25:44 +0900

    23.04.14 SpringBoot JWT 인증과 예외 처리

--- src/main/java/com/web/study/config/SecurityConfig.java
+++ src/main/java/com/web/study/config/SecurityConfig.java
@@ -5,14 +5,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.web.study.security.jwt.JwtAuthenticationEntryPoint;
+import com.web.study.security.jwt.JwtAuthenticationFilter;
+import com.web.study.security.jwt.JwtTokenProvider;
+
+import lombok.RequiredArgsConstructor;
 
 
 
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private final JwtTokenProvider jwtTokenProvider;
+	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 	
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
@@ -22,11 +34,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	// security filter
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
 		http.csrf().disable();
+		http.httpBasic().disable(); // 웹 기본 인증 방식
+		http.formLogin().disable(); // 폼태그 통한 로그인
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // 세션 비활성 (무상태성)
+		
+		//401 : 인증되지않음
+		//403 : 권한없음		
 		http.authorizeRequests()
 			.antMatchers("/auth/register/**", "/auth/login/**")
 			.permitAll()
 			.anyRequest()
-			.authenticated();
+			.authenticated()
+			.and()
+			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+			.exceptionHandling()
+			.authenticationEntryPoint(jwtAuthenticationEntryPoint);
+			
 	}
 }
